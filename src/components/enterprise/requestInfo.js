dbd8dc6e166c464419df56cabfcbc6ff4ee25071
PrasadPallaa <dvprasad.palla@gmail.com>
2022-06-06 22:06:06 +0530

    first commit

--- src/components/enterprise/requestInfo.js
+++ src/components/enterprise/requestInfo.js
@@ -40,7 +40,7 @@ const MenuProps = {
   },
 };
 
-const names = ["Service 1", "Service 2", "Service 3"];
+const names = ["Business Cloud Computing", "SIP Trunk", "Business Lines"];
 
 const servicePlanData = ["Yes", "No"];
 
@@ -59,19 +59,20 @@ const RequestInfo = () => {
   const [checked, setChecked] = React.useState(false);
   const theme = useTheme();
   const [personName, setPersonName] = React.useState([]);
-  const [firstName, setFirstName] = useState();
-  const [lastName, setLastName] = useState();
-  const [address, setAddress] = useState();
-  const [stateValue, setStateValue] = useState();
-  const [zip, setZip] = useState();
-  const [email, setEmail] = useState();
-  const [phoneNumber, setPhoneNumber] = useState();
-  const [companyName, setCompanyName] = useState();
-  const [servicePlan, setServicePlan] = useState(["Service 1"]);
-  const [noOfUsers, setNoOfUser] = useState();
-  const [internetService, setInternetService] = useState(["Yes"]);
-  const [contactAgent, setContactAgent] = useState(["Email"]);
-  const [personName1, setPersonName1] = React.useState([]);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [address, setAddress] = useState("");
+  const [stateValue, setStateValue] = useState("");
+  const [zip, setZip] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [companyName, setCompanyName] = useState("");
+  const [servicePlan, setServicePlan] = useState([]);
+  const [noOfUsers, setNoOfUser] = useState("");
+  const [internetService, setInternetService] = useState([]);
+  const [contactAgent, setContactAgent] = useState([]);
+  const [ban, setBan] = useState("");
+  const [isSubmit, setIsSubmit] = React.useState(false);
 
   const handleChange2 = (event) => {
     const {
@@ -88,6 +89,10 @@ const RequestInfo = () => {
 
   function onChange(value) {
     console.log("Captcha value:", value);
+  }
+
+  const onSubmit = () => {
+    setIsSubmit(true);
   }
 
   
@@ -114,60 +119,24 @@ const RequestInfo = () => {
           <TextField
             required
             fullWidth
-            label="First name"
+            label="First Name"
             id="firstName"
             value={firstName}
             onChange={(event) => setFirstName(event.target.value)}
-            error={firstName === ""}
-            helperText={firstName === "" ? "This field is required!" : " "}
+            error={firstName === "" && isSubmit === true}
+            helperText={firstName === "" && isSubmit === true ? "This field is required!" : " "}
           />
         </Grid>
         <Grid item xs={6}>
           <TextField
             required
             fullWidth
-            label="Last name"
+            label="Last Name"
             id="lastName"
             value={lastName}
             onChange={(event) => setLastName(event.target.value)}
-            error={lastName === ""}
-            helperText={lastName === "" ? "This field is required!" : " "}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            required
-            fullWidth
-            label="Address"
-            id="address"
-            value={address}
-            onChange={(event) => setAddress(event.target.value)}
-            error={address === ""}
-            helperText={address === "" ? "This field is required!" : " "}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            required
-            fullWidth
-            label="State"
-            id="state"
-            value={stateValue}
-            onChange={(event) => setStateValue(event.target.value)}
-            error={stateValue === ""}
-            helperText={stateValue === "" ? "This field is required!" : " "}
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            required
-            fullWidth
-            label="Zip/Postal Code"
-            id="zip"
-            value={zip}
-            onChange={(event) => setZip(event.target.value)}
-            error={zip === ""}
-            helperText={zip === "" ? "This field is required!" : " "}
+            error={lastName === "" && isSubmit === true}
+            helperText={lastName === "" && isSubmit === true ? "This field is required!" : " "}
           />
         </Grid>
         <Grid item xs={6}>
@@ -178,22 +147,11 @@ const RequestInfo = () => {
             id="email"
             value={email}
             onChange={(event) => setEmail(event.target.value)}
-            error={email === ""}
-            helperText={email === "" ? "This field is required!" : " "}
+            error={email === "" && isSubmit === true}
+            helperText={email === "" && isSubmit === true ? "This field is required!" : " "}
           />
         </Grid>
-        <Grid item xs={6}>
-          <TextField
-            required
-            fullWidth
-            label="Phone Number"
-            id="phoneNumber"
-            value={phoneNumber}
-            onChange={(event) => setPhoneNumber(event.target.value)}
-            error={phoneNumber === ""}
-            helperText={phoneNumber === "" ? "This field is required!" : " "}
-          />
-        </Grid>
+
         <Grid item xs={6}>
           <TextField
             fullWidth
@@ -202,10 +160,61 @@ const RequestInfo = () => {
             id="companyName"
             value={companyName}
             onChange={(event) => setCompanyName(event.target.value)}
-            error={companyName === ""}
-            helperText={companyName === "" ? "This field is required!" : " "}
+            error={companyName === "" && isSubmit === true}
+            helperText={companyName === "" && isSubmit === true ? "This field is required!" : " "}
           />
         </Grid>
+
+        <Grid item xs={6}>
+          <TextField
+            required
+            fullWidth
+            label="Address"
+            id="address"
+            value={address}
+            onChange={(event) => setAddress(event.target.value)}
+            error={address === "" && isSubmit === true}
+            helperText={address === "" && isSubmit === true ? "This field is required!" : " "}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            required
+            fullWidth
+            label="State"
+            id="state"
+            value={stateValue}
+            onChange={(event) => setStateValue(event.target.value)}
+            error={stateValue === "" && isSubmit === true}
+            helperText={stateValue === "" && isSubmit === true ? "This field is required!" : " "}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <TextField
+            required
+            fullWidth
+            label="Zip/Postal Code"
+            id="zip"
+            value={zip}
+            onChange={(event) => setZip(event.target.value)}
+            error={zip === "" && isSubmit === true}
+            helperText={zip === ""  && isSubmit === true ? "This field is required!" : " "}
+          />
+        </Grid>
+
+        <Grid item xs={6}>
+          <TextField
+            required
+            fullWidth
+            label="Phone Number"
+            id="phoneNumber"
+            value={phoneNumber}
+            onChange={(event) => setPhoneNumber(event.target.value)}
+            error={phoneNumber === "" && isSubmit === true}
+            helperText={phoneNumber === "" && isSubmit === true ? "This field is required!" : " "}
+          />
+        </Grid>
+
         <Grid item xs={6}>
           {/* <FormControl sx={{ width: 640 }} error={servicePlan.length === 0}>
             <InputLabel id="demo-multiple-checkbox-label">
@@ -241,7 +250,9 @@ const RequestInfo = () => {
             )}
           </FormControl> */}
 
-          <FormControl sx={{ width: 640 }} error={servicePlan.length === 0}>
+{/* error={servicePlan.length === 0} */}
+
+          <FormControl sx={{ width: 640 }} error={servicePlan.length === 0 && isSubmit == true}>
             <InputLabel id="demo-multiple-checkbox-label">Service Plan</InputLabel>
             <Select
               labelId="demo-multiple-checkbox-label"
@@ -252,6 +263,7 @@ const RequestInfo = () => {
               input={<OutlinedInput label="Service Plan" />}
               renderValue={(selected) => selected.join(", ")}
               MenuProps={MenuProps}
+              placeholder="....."
             >
               {names.map((name) => (
                 <MenuItem key={name} value={name}>
@@ -260,7 +272,7 @@ const RequestInfo = () => {
                 </MenuItem>
               ))}
             </Select>
-            {servicePlan.length === 0 ? (
+            {servicePlan.length === 0 && isSubmit == true? (
               <FormHelperText>This field is required!</FormHelperText>
             ) : (
               ""
@@ -275,12 +287,12 @@ const RequestInfo = () => {
             id="users"
             value={noOfUsers}
             onChange={(event) => setNoOfUser(event.target.value)}
-            error={noOfUsers === ""}
-            helperText={noOfUsers === "" ? "This field is required!" : " "}
+            error={noOfUsers === ""  && isSubmit === true}
+            helperText={noOfUsers === "" && isSubmit === true ? "This field is required!" : " "}
           />
         </Grid>
         <Grid item xs={6}>
-          <FormControl sx={{ width: 640 }} error={internetService.length === 0}>
+          <FormControl sx={{ width: 640 }} error={internetService.length === 0  && isSubmit == true}>
             <InputLabel id="demo-multiple-name-label">
               Do you currently have internet service ? *
             </InputLabel>
@@ -307,7 +319,7 @@ const RequestInfo = () => {
                 </MenuItem>
               ))}
             </Select>
-            {internetService.length === 0 ? (
+            {internetService.length === 0  && isSubmit == true ? (
               <FormHelperText>This field is required!</FormHelperText>
             ) : (
               ""
@@ -317,7 +329,7 @@ const RequestInfo = () => {
 
         <Grid item xs={6}>
           {!checked ? (
-            <FormControl sx={{ width: 640 }} error={contactAgent.length === 0}>
+            <FormControl sx={{ width: 640 }} error={contactAgent.length === 0 && isSubmit == true}>
               <InputLabel id="demo-multiple-name-label">
                 How would you like our sales agent to contact you ? *
               </InputLabel>
@@ -344,14 +356,25 @@ const RequestInfo = () => {
                   </MenuItem>
                 ))}
               </Select>
-              {contactAgent.length === 0 ? (
+              {contactAgent.length === 0  && isSubmit == true ? (
                 <FormHelperText>This field is required!</FormHelperText>
               ) : (
                 ""
               )}
             </FormControl>
           ) : (
-            <TextField required fullWidth label="BAN" id="ban" type="number" />
+            // <TextField required fullWidth label="BAN" id="ban" type="number" />
+            <TextField
+            fullWidth
+            required
+            type="number"
+            label="BAN"
+            id="ban"
+            value={ban}
+            onChange={(event) => setBan(event.target.value)}
+            error={ban === "" && isSubmit === true}
+            helperText={ban === "" && isSubmit === true ? "This field is required!" : " "}
+          />
           )}
           <FormGroup>
             <FormControlLabel
@@ -391,7 +414,7 @@ const RequestInfo = () => {
           />
         </Grid>
         <Grid item xs={12} className="quote-button">
-          <Button className="quote-button-quote">Submit Info</Button>
+          <Button className="quote-button-quote" onClick={onSubmit}>Submit Info</Button>
         </Grid>
         <Grid item xs={12} className="quote-button">
           <p>
