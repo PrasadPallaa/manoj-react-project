df515541aa5d95d18a77c3ba656a46824913aab0
PrasadPallaa <dvprasad.palla@gmail.com>
2022-06-10 09:25:12 +0530

    commit

---
+++ src/components/login/login.js
@@ -0,0 +1,75 @@
+import Grid from "@mui/material/Grid";
+import brspdlogo from "../../images/Branding/01 Logo/Logo/Full Color/Digital/Brightspeed_Logo_Full_Color_RGB_864px@72ppi.png";
+import loginpageImage from "../../images/Branding/Image Library/Brightspeed Image Library_Commercial_Retouched/RF_GettyImages-1359098771/RF_GettyImages-1359098771_1.png";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import React, { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSubmit, setIsSubmit] = React.useState(false);
+
+  const onSubmit = () => {
+    setIsSubmit(true);
+  };
+  return (
+    <>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <img src={brspdlogo} alt="logo" className="img-brightspeed-login" />
+          <img
+            src={loginpageImage}
+            style={{ height: "90.7%", width: "100%" }}
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <div className="login-form">
+            <Grid item>
+              <TextField
+                required
+                style={{ width: "92%" }}
+                label="Email"
+                id="Email"
+                value={email}
+                onChange={(event) => setEmail(event.target.value)}
+                error={email === "" && isSubmit === true}
+                helperText={
+                  email === "" && isSubmit === true ? "Email is required!" : " "
+                }
+              />
+            </Grid>
+            <Grid item className="login-password">
+              <TextField
+                required
+                style={{ width: "92%" }}
+                label="Password"
+                id="Password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+                error={password === "" && isSubmit === true}
+                helperText={
+                  password === "" && isSubmit === true
+                    ? "Password is required!"
+                    : " "
+                }
+              />
+            </Grid>
+            <Grid item xs={11}>
+              <label className="forget-password">
+                <a className="forgt-pass-link">Forget Password</a>
+              </label>
+              </Grid>
+            <Grid item className="login-button">
+              <Button className="login-button-login" onClick={onSubmit}>
+                Login
+              </Button>
+            </Grid>
+          </div>
+        </Grid>
+      </Grid>
+    </>
+  );
+};
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -5,37 +5,44 @@ import Home from "./pages/Home";
 import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
-import CompanyHeader from "./pages/CompanyHeader"
-import Enterprisevoice from "./components/enterprise/enterprisevoice"
-import Productctlg from "./components/enterprise/productctlg"
-import 'bootstrap/dist/css/bootstrap.min.css';
+import CompanyHeader from "./pages/CompanyHeader";
+import Enterprisevoice from "./components/enterprise/enterprisevoice";
+import Productctlg from "./components/enterprise/productctlg";
+import "bootstrap/dist/css/bootstrap.min.css";
 import MidEnterprise from "./pages/MidEnterprise";
 import Navbar from "./components/navbar";
-import WholeSale from './components/wholesale/wholesale'
-import Voice from './components/enterprise/voice'
+import WholeSale from "./components/wholesale/wholesale";
+import Voice from "./components/enterprise/voice";
+import Login from "./components/login/login";
 
 
-export default function App() {
+export default function App(props) {
+  const pathname = window.location.pathname;
   return (
-    <BrowserRouter>
-    <Navbar />
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="blogs" element={<Blogs />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="companyheader" element={<CompanyHeader />} />
-          <Route path="ew/enterprise" element={<Enterprisevoice />} />
-          <Route path="ew/enterprise/voice" element={<Voice />} />
-          <Route path="ew/wholesale/voice" element={<WholeSale />} />
-          <Route path="mid-enterprise" element={<MidEnterprise />} />
-          <Route path="products" element={<Productctlg />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        {pathname !== "/login" && <Navbar />}
+        <Routes>
+          {pathname !== "/login" && (
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="blogs" element={<Blogs />} />
+              <Route path="contact" element={<Contact />} />
+              <Route path="companyheader" element={<CompanyHeader />} />
+              <Route path="ew/enterprise" element={<Enterprisevoice />} />
+              <Route path="ew/enterprise/voice" element={<Voice />} />
+              <Route path="ew/wholesale/voice" element={<WholeSale />} />
+              <Route path="mid-enterprise" element={<MidEnterprise />} />
+              <Route path="products" element={<Productctlg />} />
+              <Route path="*" element={<NoPage />} />
+            </Route>
+          )}
+          <Route path="login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
